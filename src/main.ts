2429ac6e7dd7760e42973368d933ccbb85fe4015
heroyooi <heroyooi1018@gmail.com>
2023-04-28 10:46:37 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './index.css'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1 class="text-3xl font-bold">Hello world</h1>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
